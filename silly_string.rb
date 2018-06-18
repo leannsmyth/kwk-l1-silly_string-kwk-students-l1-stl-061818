2ead198c59299cd4e21c27e92d8c5402c7fb00be
leannsmyth <leannmsmyth@gmail.com>
2018-06-18 21:12:17 +0000

    automatically backed up by learn

--- silly_string.rb
+++ silly_string.rb
@@ -1,51 +1,51 @@
 
-# Challenge 1: Fill this string in with your favorite word
+# # Challenge 1: Fill this string in with your favorite word
 
-your_favorite_word = "dog"
+# your_favorite_word = "dog"
 
-puts "dog" #{dog}"
+# puts "dog" #{dog}"
 
-"dog".upcase # Challenge 2: Capitalize every letter in your_favorite_word using a built in Ruby method
+# "dog".upcase # Challenge 2: Capitalize every letter in your_favorite_word using a built in Ruby method
 
-"dog" .upcase = DOG # Put a . after your_favorite word and use a String method.
+# "dog" .upcase = DOG # Put a . after your_favorite word and use a String method.
 
-puts "dog" #{DOG}"
+# puts "dog" #{DOG}"
 
-# Challenge 3: Make all the letters of phrase lowercase using a built in Ruby method
+# # Challenge 3: Make all the letters of phrase lowercase using a built in Ruby method
 
-phrase = "IM NOT SHOUTING" .downcase
+# phrase = "IM NOT SHOUTING" .downcase
 
-lowercased_phrase = phrase # Put a . after phrase and use a String method.
+# lowercased_phrase = phrase # Put a . after phrase and use a String method.
 
-puts lowercased_phrase
+# puts lowercased_phrase
 
-# Challenge 4: Count how many letters are in that big_word
+# # Challenge 4: Count how many letters are in that big_word
 
-big_word = "supercalifragilisticexpialidocious" .length
+# big_word = "supercalifragilisticexpialidocious" .length
 
-letter_count = big_word # Put a . after big_word and use a String method.
+# letter_count = big_word # Put a . after big_word and use a String method.
 
-puts "There are #{letter_count} in #{big_word}"
+# puts "There are #{letter_count} in #{big_word}"
 
-# Challenge 5: Add "wow" and "mom" to big_word
+# # Challenge 5: Add "wow" and "mom" to big_word
 
-bigger_word = big_word # Use String concatenation to add "wow" and "mom" to big_word
+# bigger_word = big_word # Use String concatenation to add "wow" and "mom" to big_word
 
-new_letter_count = bigger_word.size
+# new_letter_count = bigger_word.size
 
-puts "There are now #{new_letter_count} in #{bigger_word}"
+# puts "There are now #{new_letter_count} in #{bigger_word}"
 
-# Challenge 6: Capitalize this sentence properly
+# # Challenge 6: Capitalize this sentence properly
 
-proper_sentence = "i really like programming." # Put a . after the string to capitalize it properly.
+# proper_sentence = "i really like programming." # Put a . after the string to capitalize it properly.
 
-puts proper_sentence
+# puts proper_sentence
 
-# Challenge 7: String Switcheroo
-sentence = "Astounding aardvarks, arguably an ancient animal, always ate apples and acorns and artichokes all around Athens, amazing!"
+# # Challenge 7: String Switcheroo
+# sentence = "Astounding aardvarks, arguably an ancient animal, always ate apples and acorns and artichokes all around Athens, amazing!"
 
-# Swap all `A` for `O`, as well as all `a` for `o`. You need to chain `gsub`s.
+# # Swap all `A` for `O`, as well as all `a` for `o`. You need to chain `gsub`s.
 
-sentence = sentence # Add your gsub chain here
+# sentence = sentence # Add your gsub chain here
 
-puts sentence
+# puts sentence
