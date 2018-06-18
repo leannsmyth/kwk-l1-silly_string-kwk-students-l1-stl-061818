72cef8677e31222f14128e800f37c61700352e99
leannsmyth <leannmsmyth@gmail.com>
2018-06-18 21:24:38 +0000

    automatically backed up by learn

--- silly_string.rb
+++ silly_string.rb
@@ -57,7 +57,8 @@ puts "IM NOT SHOUTING" .downcase
 puts "supercalifragilisticexpialidocious" .length
 puts "wow"+"mom"
 puts "i really like programming" .capitalize
-puts "Astounding aardvarks, arguably an ancient animal, always ate apples and acorns and artichokes all around Athens, amazing!" .gsub("A, a", "")
+puts "Astounding aardvarks, arguably an ancient animal, always ate apples and acorns and artichokes all around Athens, amazing!" .gsub("A, a", "o")
+
 
 
 
