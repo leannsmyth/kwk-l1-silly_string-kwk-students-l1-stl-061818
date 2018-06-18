d0bf577978b71ed4fc5b79418b9dffc9a3ad632a
leannsmyth <leannmsmyth@gmail.com>
2018-06-18 21:28:14 +0000

    automatically backed up by learn

--- silly_string.rb
+++ silly_string.rb
@@ -51,13 +51,17 @@
 # puts sentence
 
 
-favorite word = "dog"
-puts "dog" .upcase
+def method
+favorite_word = "dog"
+puts "dog".upcase
 puts "IM NOT SHOUTING" .downcase
 puts "supercalifragilisticexpialidocious" .length
 puts "wow"+"mom"
 puts "i really like programming" .capitalize
 puts "Astounding aardvarks, arguably an ancient animal, always ate apples and acorns and artichokes all around Athens, amazing!" .gsub("A, a", "o")
+end
+
+method
 
 
 
